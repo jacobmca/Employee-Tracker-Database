0f4d7548601d07227c33fcdf5d3f1ad086fef1e9
Jacob McAuliffe <64445624+jacobmca@users.noreply.github.com>
2024-06-20 08:56:14 -0700

    Created basic file structure for the project. Installed inquirer, postgres, and nodemon. Created the Departments, Roles, and Employees databases in schema.sql.

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+-- Create Departments Database
+
+DROP DATABASE IF EXISTS departments.db;
+CREATE DATABASE departments.db;
+
+\c departments.db;
+
+-- Create Roles Database
+
+DROP DATABASE IF EXISTS roles.db;
+CREATE DATABASE roles.db;
+
+\c roles.db;
+
+-- Create Employees Database
+
+DROP DATABASE IF EXISTS employees.db;
+CREATE DATABASE employees.db;
+
+\c employees.db;
